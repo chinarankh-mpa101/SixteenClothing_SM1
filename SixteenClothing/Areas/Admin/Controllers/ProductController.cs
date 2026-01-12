9d12aa3fad20bc414bf7fe83ae45c59155e3fa93
“Chinara <311.kamran@gmail.com>
2026-01-12 23:58:33 +0400

    admin product ready

---
+++ SixteenClothing/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SixteenClothing.Contexts;
+using SixteenClothing.ViewModels.ProductViewModels;
+
+namespace SixteenClothing.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController(AppDbContext _context) : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var products = await _context.Products.Select(x=>new ProductGetVM
+            {
+                Id=x.Id,
+                Name=x.Name,
+                Description=x.Description,
+                ImagePath=x.ImagePath,
+                Price=x.Price,
+                Rating=x.Rating,
+                CategoryName=x.Category.Name
+            }).ToListAsync();
+            return View(products);
+        }
+    }
+}
---
+++ SixteenClothing/ViewModels/ProductViewModels/ProductGetVM.cs
@@ -0,0 +1,13 @@
+﻿namespace SixteenClothing.ViewModels.ProductViewModels
+{
+    public class ProductGetVM
+    {
+        public int Id { get; set; }
+        public string Name { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public string ImagePath { get; set; } = string.Empty;
+        public decimal Price { get; set; }
+        public int Rating { get; set; }
+        public string CategoryName { get; set; } = string.Empty;
+    }
+}
